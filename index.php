f5e667a61ad98b95edea7a31b2cdda1371b141c3
adruijter <adruijter@gmail.com>
2024-11-27 11:59:09 +0100

    create.php aangemaakt

--- index.php
+++ index.php
@@ -68,14 +68,21 @@ $result = $statement->fetchAll(PDO::FETCH_OBJ);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hoogste Achtbanen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
-    <div class="container">
+    <div class="container mt-3">
         
         <div class="row">
             <div class="col-2"></div>
-            <div class="col-8"><h3>Hoogste achtbanen van Europa</h3></div>
+            <div class="col-8"><h3 class="text-primary">Hoogste achtbanen van Europa</h3></div>
             <div class="col-2"></div>
+        </div>
+
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-2"><h6>Nieuwe achtbaan <a href="./create.php"><i class="bi bi-plus-square text-danger"></i></a></h5></div>
+          <div class="col-2"></div>
         </div>
 
         <div class="row">
